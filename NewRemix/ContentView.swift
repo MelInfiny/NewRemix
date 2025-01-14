b9a930ccc0082dcc459285bebb58284dc0e42364
MelInfiny <70651843+MelInfiny@users.noreply.github.com>
2025-01-14 11:36:47 +0100

    Initial Commit

---
+++ NewRemix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewRemix
+//
+//  Created by m1 on 14/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewRemix/NewRemixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewRemixApp.swift
+//  NewRemix
+//
+//  Created by m1 on 14/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NewRemixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
